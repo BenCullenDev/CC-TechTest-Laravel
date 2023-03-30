41f7c59104d8b138a0a131f6ccf32639da004281
Louis <louis@cacode.co.uk>
2023-03-30 17:20:47 +0100

    Ready, Set, Go!

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>CarbonCode - Laravel Tech Test</title>
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    </head>
+    <body style="font-family: 'figtree', 'Arial';">
+        <h1>CarbonCode - Laravel Tech Test</h1>
+        <p>See the README.md for information :)</p>
+    </body>
+</html>
